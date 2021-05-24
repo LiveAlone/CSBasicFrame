c0e9fc23aad8d634d3ee509b7422dd5ffd53d054
qijunyao <qijunyao@xiaohongshu.com>
2021-05-24 18:20:01 +0800

    add all

---
+++ aio/src/main/java/org/yqj/net/aio/EmptyMain.java
@@ -0,0 +1,15 @@
+package org.yqj.net.aio;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Description:
+ *
+ * @author yaoqijun
+ * @date 2021/5/21
+ * Email: yaoqijunmail@foxmail.com
+ */
+@Slf4j
+public class EmptyMain {
+
+}
